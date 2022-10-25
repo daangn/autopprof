a18096bbb3ca6c60203e2c883c5155be6fccf7bf
Ingyu Hwang (Evan) <dlsrb6342@gmail.com>
2022-10-25 17:52:32 +0900

    fix: fix unsupported platform methods (#4)
    * fix: add Stop() method to unsupported
    
    * use value

--- autopprof_unsupported.go
+++ autopprof_unsupported.go
@@ -4,6 +4,9 @@
 package autopprof
 
 // Start does not do anything on unsupported platforms.
-func Start(opt *Option) error {
+func Start(opt Option) error {
 	return ErrUnsupportedPlatform
 }
+
+// Stop does not do anything on unsupported platforms.
+func Stop() {}
