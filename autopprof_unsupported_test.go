77dc921e8b11486e5f95b9a8afa3dc32c58f6bd9
MinJae Kwon <mingrammer@gmail.com>
2022-11-02 14:10:50 +0900

    fix(test): broken unsupported test

--- autopprof_unsupported_test.go
+++ autopprof_unsupported_test.go
@@ -11,18 +11,18 @@ import (
 func TestStart(t *testing.T) {
 	testCases := []struct {
 		name string
-		opt  *Option
+		opt  Option
 		want error
 	}{
 		{
 			name: "unsupported platform",
-			opt:  nil,
+			opt:  Option{},
 			want: ErrUnsupportedPlatform,
 		},
 	}
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			if err := Start(tc.opt); errors.Is(err, tc.want) {
+			if err := Start(tc.opt); !errors.Is(err, tc.want) {
 				t.Errorf("Start() = %v, want %v", err, tc.want)
 			}
 		})
